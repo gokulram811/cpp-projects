c0c8a4fc4df8dc6dce84aed3d76625c09d5d3de4
gokulram811 <gokulrammanivannan@gmail.com>
2026-02-18 09:30:15 +0530

    replacing old  code with new version

--- man.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "TrigMath.hpp"
-
-int main() {
-    double angle = 90.0;
-    std::cout << "Sin of " << angle << " degrees: " << Trig::sin(angle, true) << std::endl;
-    std::cout << "Cosec of 0.5 rad: " << Trig::cosec(0.5) << std::endl;
-    return 0;
-}
